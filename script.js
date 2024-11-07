624248cc85a396f661b39e73dd02cfa84f1f4979
bobby-codes-stuff <bobbysun.150801@gmail.com>
2024-11-07 13:29:50 +1300

    finished logic for rock paper scissors

--- script.js
+++ script.js
@@ -25,27 +25,49 @@ function getHumanChoice() {
 // function that plays a match of rock paper scissors
 // switch case breaks at end: https://stackoverflow.com/questions/65455535/every-case-in-switch-is-triggered-in-js#:~:text=JavaScript%20supports%20C%20style%20switch,must%20end%20it%20with%20break%3B%20.
 // JavaScript supports C style switch case fall through, which means unless there is a break specified, it will continue to execute all the subsequent cases.
+
+// +3 added to make all negatives their respective positive value.
 function playRound(humanChoice, computerChoice) {
     console.log(`You selected "${Object.keys(Choices).find(key => Choices[key] === humanChoice)}" and the computer selected "${Object.keys(Choices).find(key => Choices[key] === computerChoice)}".`);
     switch((humanChoice-computerChoice+3)%3) {
         case 0:
             // tie
-            console.log("It's a tie! Try again!");
+            console.log("It's a tie!");
             break;
         case 1:
             // player wins
-            console.log("You won! Woo!");
+            console.log("You won!");
             humanScore++;
             break;
         case 2:
             // computer wins
-            console.log("You lost. Better luck next time!");
+            console.log("You lost.");
             computerScore++;
     }
+    console.log(`Score (You - Computer): ${humanScore} - ${computerScore}`)
 
+}
+
+// BO5 rounds
+function playGame() {
+    for (let i=0;i<5;i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice(); 
+        playRound(humanSelection, computerSelection);
+    }
+    console.log(`Final Score (You - Computer): ${humanScore} - ${computerScore}`)
+    if (humanScore > computerScore) {
+        console.log("You Win!");
+    }
+    else if (humanScore < computerScore) {
+        console.log("You Lost.");
+    }
+    else {
+        console.log("It's a Draw!");
+    }
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
-console.log(playRound(getHumanChoice(), getComputerChoice()))
+playGame()
